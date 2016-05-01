0931592ce1075940f312517db53ab126d9124667
Julen Pardo <julen.pardo@alumni.mondragon.edu>
2016-05-01 19:33:32 +0200

    dev-properties: test multicast ip checker

--- test/com/github/julenpardo/jmiddleware/properties/PropertiesCheckerMulticastIpTest.java
+++ test/com/github/julenpardo/jmiddleware/properties/PropertiesCheckerMulticastIpTest.java
@@ -1,20 +1,63 @@
 package com.github.julenpardo.jmiddleware.properties;
 
+import org.junit.Assume;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.fail;
+
+@RunWith(Parameterized.class)
 public class PropertiesCheckerMulticastIpTest {
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    Object[][] data = new Object[][] {
+            {"224.0.0.0", false},
+            {"224.0.0.1", false},
+            {"230.0.0.0", false},
+            {"239.255.255.253", false},
+            {"239.255.255.254", false},
+            {"223.255.255.255", true},
+            {"220.0.0.0", true},
+            {"239.255.255.255", true},
+            {"250.0.0.0", true},
+    };
+
+    return Arrays.asList(data);
+  }
+
+  private String input;
+  private boolean exceptionExpected;
+
+  public PropertiesCheckerMulticastIpTest(String input, boolean exceptionExpected) {
+    this.input = input;
+    this.exceptionExpected = exceptionExpected;
+  }
 
   @Test
   public void checkMulticastIpTest() {
-
-  }
-
-  public void checkMulticastIpTestInvalidPropertiesException() {
-
+    if (!this.exceptionExpected) {
+      try {
+        PropertiesChecker.checkMulticastIp(this.input);
+      } catch (InvalidPropertiesException e) {
+        fail("No exception should be thrown.");
+      }
+    }
   }
 
   @Test
-  public void invalidOctect() {
-
+  public void checkMulticastIpTestInvalidPropertiesException() {
+    if (this.exceptionExpected) {
+      try {
+        PropertiesChecker.checkMulticastIp(this.input);
+        fail("Exception should be thrown.");
+      } catch (InvalidPropertiesException e) { }
+    }
   }
+
 }
